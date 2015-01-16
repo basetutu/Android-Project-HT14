eea9f6ac09ae61d5b609810024796ee6b6574ad0
Saeed Ghasemi <basetutu@gmail.com>
2015-01-16 15:16:07 +0100

    tests

--- app/src/main/java/com/malmo_university/mylists/MainActivity.java
+++ app/src/main/java/com/malmo_university/mylists/MainActivity.java
@@ -48,8 +48,9 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        
+
         mViewPager = (ViewPager) findViewById(R.id.pager);
+
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         // When swiping between different sections, select the corresponding
