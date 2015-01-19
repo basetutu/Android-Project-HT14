515f1e76d0c86f088618b89e43e092eff7b17a12
Jonathan <jonathanbjarnason@gmail.com>
2015-01-19 17:34:48 +0100

    Added array adaptor to fragment checklist.

--- app/src/main/java/com/malmo_university/mylists/FragmentChecklists.java
+++ app/src/main/java/com/malmo_university/mylists/FragmentChecklists.java
@@ -6,11 +6,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class FragmentChecklists extends Fragment {
+    String[] checklists;
+    View[] checklists_v;
+    TextView txtView;
+
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -30,18 +38,51 @@ public class FragmentChecklists extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        Log.w("fsdf", "onCreateView");
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        return rootView;
+    public void onCreate(Bundle savedInstanceState) {
+        checklists = new String[] {"potatis","mjölk"};
+        super.onCreate(savedInstanceState);
+        Log.w("fsdf","onCreate");
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.w("fsdf","onCreate");
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.w("fsdf", "onCreateView");
+        View rootView = inflater.inflate(R.layout.fragment_checklists, container, false);
+        View checklistItemLeftView = inflater.inflate(R.layout.listview_item_chat_left, container, false);
+        View checklistItemRightView = inflater.inflate(R.layout.listview_item_chat_right, container, false);
+        checklists_v = new View[] {checklistItemLeftView, checklistItemRightView};
+
+        /////
+
+        ArrayAdapter<View> adapter = new ArrayAdapter<View>(getActivity(),
+                android.R.layout.simple_list_item_1, checklists_v);
+        ListView list = (ListView) rootView.findViewById(R.id.listview_checklists);
+        list.setAdapter(adapter);
+        /*
+        txtView = (TextView) rootView.findViewById(R.id.textView_GroupList1);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // google fragment new instance
+
+                if(position == 0){
+                    txtView.setText("potatis");
+                }
+                else {
+                    Fragment fragment = new ChatScreen();
+                    getFragmentManager().beginTransaction().replace(R.id.container2, fragment)
+                            .addToBackStack(null).commit();
+                }
+            }
+        });
+        */
+        ////
+
+        return rootView;
     }
 
     @Override
